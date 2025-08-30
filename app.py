5e2f8d5c13797e08d69937b2cad3488f59b6fd6c
Kishore R <kishorerajaji65@gmail.com>
2025-08-30 14:49:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+
+model , scaler = pickle.load(open('trained_model.sav', 'rb'))
+
+
+st.title("Obesity Prediction")
+
+st.write("Enter the details below to predict")
+
+Age = st.number_input('Age:')
+Gender = st.number_input('Gender (Male : 0 ,Female : 1):')
+Height=st.number_input('Height:')
+Weight=st.number_input('Weight:')
+BMI=st.number_input('BMI:')
+PhysicalActivityLevel=st.number_input('PhysicalActivityLevel:')
+
+features = np.array([[Age,Gender,Height,Weight,BMI,PhysicalActivityLevel]])
+
+features_scaled = scaler.transform(features)
+
+if st.button("Obesity Price"):
+    prediction = model.predict(features_scaled)
+    st.success(f"Estimated Category: {prediction[0]:,.2f}k")
